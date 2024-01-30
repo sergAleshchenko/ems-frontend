368caf3dc30ebf98fb6ed33dcede5ff3b3135bc7
Sergei Aleshchenko <serg.aleshchenko@gmail.com>
2024-01-31 02:47:41 +0400

    Continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-        <ListEmployeeComponent />
+    return (
+        <>
+            <BrowserRouter>
+                <HeaderComponent/>
+                {/*// http://localhost:3000*/}
 
-    </>
-  )
+                <Routes>
+                    <Route path='/' element={<ListEmployeeComponent/>}></Route>
+
+                    {/*// http://localhost:3000/employees*/}
+                    <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+                </Routes>
+                <FooterComponent/>
+            </BrowserRouter>
+        </>
+    );
+
+
 }
 
 export default App
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className='navbar-dark bg-dark'>
+                    <a className='navbar-brand' href="https://www.javaguides.net">
+                        Employee Management System
+                    </a>
+                </nav>
+            </header>
+
+        </div>
+    )
+}
+export default HeaderComponent
